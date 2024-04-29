c48439c2ce49db2692a2dda33cacca4727d4787e
Shyanne <Zering130@stcharles.k12.la.us>
2024-04-29 17:31:53 +0000

    saving sorting-exercise

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-### HIIIII ![whale shark](whale-shark.png)
+### HIIIII ![whale shark](/workspace/Shyanneringue.github.io/whale-shark.png)
 
 - ![coding shark](sharks/code.png) I’m currently working on ... shyanneringue.github.io
-- ![nerd shark](sharks/nerd.png) I’m currently learning ... how to make a website using JavaScript, HTML, and CSS.
+- ![nerd shark](https://github.com/Shyanneringue/Shyanneringue.github.io/blob/main/sharks/nerd.png) I’m currently learning ... how to make a website using JavaScript, HTML, and CSS.
 - ![fish and shark](sharks/fish.png) I’m looking to collaborate on ... anything
 - ![hmm shark](sharks/hmm.png) I’m looking for help with ... how to make these pictures load help
-- ![speach bubble shark](sharks/talk.png) Ask me about ... anything
+- ![speech bubble shark](sharks/talk.png) Ask me about ... anything
 - ![newspaper shark](sharks/news.png) How to reach me: ... Zering130.stcharles.k12.la.us
 - ![headphone shark](sharks/headphones.png) Pronouns: ... she/her
 - ![thumbs up shark](sharks/thumbs.png) Fun fact: ... shark
--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -15,6 +15,18 @@ The CSS ids you will work with are:
 
 // TODO 2: Implement bubbleSort
 
+// sorts all elements of the provided array from smallest to largest
+async function bubbleSort(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = array.length - 1; j = i + 1; j--) {
+            if (array[j] < array[j - 1]) {
+                swap(array, array[j], array[j - 1]);
+                updateCounter(bubbleCounter);
+                await sleep();
+            }
+        }
+    }
+}
 
 // TODO 3: Implement quickSort
 
@@ -24,6 +36,12 @@ The CSS ids you will work with are:
 
 // TODO 1: Implement swap
 
+// switches two elements of the provided array
+function swap(array, i, j) {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
 
 ///////////////////////////////////////////////////////////////////////
 /////////////////////// YOUR WORK GOES ABOVE HERE /////////////////////
@@ -32,12 +50,12 @@ The CSS ids you will work with are:
 //////////////////////////// HELPER FUNCTIONS /////////////////////////
 
 // this function makes the program pause by SLEEP_AMOUNT milliseconds whenever it is called
-function sleep(){
+function sleep() {
     return new Promise(resolve => setTimeout(resolve, SLEEP_AMOUNT));
 }
 
 // This function draws the swap on the screen
-function drawSwap(array, i, j){
+function drawSwap(array, i, j) {
     let element1 = array[i];
     let element2 = array[j];
 
@@ -48,6 +66,6 @@ function drawSwap(array, i, j){
 }
 
 // This function updates the specified counter
-function updateCounter(counter){
+function updateCounter(counter) {
     $(counter).text("Move Count: " + (parseFloat($(counter).text().replace(/^\D+/g, '')) + 1));
 }
